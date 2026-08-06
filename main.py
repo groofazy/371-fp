9ccacf7c6a33211e62b6f1de5d43a9162402fd15
iunrr <matt.sant143@gmail.com>
2026-08-06 16:37:58 -0700

    WIP: Add basic webserver script

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-from socket import *
+import socket
+
+SERVER_HOST = "127.0.0.1"
+SERVER_PORT = 8080
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+server_socket.bind((SERVER_HOST, SERVER_PORT))
+
+server_socket.listen(5)
+
+print(f"Listening on port {SERVER_PORT} ...")
+
+while True:
+    client_socket, client_address = server_socket.accept()
+    request = client_socket.recv(1500).decode()
+    print(request)
