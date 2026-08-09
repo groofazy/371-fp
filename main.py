a5bf06bc3f6f46bab1131037dc5a7488ecaf4804
iunrr <matt.sant143@gmail.com>
2026-08-08 20:30:43 -0700

    Finish basic web server

--- main.py
+++ main.py
@@ -16,3 +16,17 @@ while True:
     client_socket, client_address = server_socket.accept()
     request = client_socket.recv(1500).decode()
     print(request)
+    reqHeaders = request.split('\n')
+    reqFirstLine = reqHeaders[0].split()
+
+    httpMethod = reqFirstLine[0]
+    reqPath = reqFirstLine[1]
+
+    if reqPath == '/test.html':
+        page = open('test.html')
+        content = page.read()
+        page.close()
+
+        response = 'HTTP/1.1 200 OK \n\n' + content
+        client_socket.sendall(response.encode())
+        client_socket.close()
