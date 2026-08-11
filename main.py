dbe80887c6b1d043d0ea7f5182ba3b9de743a8fc
groofazy <gabe.gamboa05@gmail.com>
2026-08-10 22:48:23 -0700

    multithread version

--- main.py
+++ main.py
@@ -1,20 +1,11 @@
 import socket
 import datetime
+import threading
 
 SERVER_HOST = "127.0.0.1"
 SERVER_PORT = 8080
 
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
-server_socket.bind((SERVER_HOST, SERVER_PORT))
-
-server_socket.listen(5)
-
-print(f"Listening on port {SERVER_PORT} ...")
-
-while True:
-    client_socket, client_address = server_socket.accept()
+def clientHandler(client_socket, client_address):
     request = client_socket.recv(1500).decode(encoding="utf")
     print(request)
     reqHeaders = request.split('\n')
@@ -77,4 +68,16 @@ while True:
     client_socket.close()
 
 
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
+server_socket.bind((SERVER_HOST, SERVER_PORT))
+
+server_socket.listen(5)
+
+print(f"Listening on port {SERVER_PORT} ...")
+
+while True:
+    client_socket, client_address = server_socket.accept()
+    t = threading.Thread(target=clientHandler, args=(client_socket, client_address), daemon=True)
+    t.start()
